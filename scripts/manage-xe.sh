7a0db8c2eb53d712a503e265f2ccdf02d2bbdf55
Karsten Ludwig Hauser <KHauser@intershop.com>
2021-11-17 11:27:21 +0100

    Fixes testcontainers problem (Timed out waiting for log output matching '.*DATABASE IS READY TO USE!.*\s')

--- scripts/manage-xe.sh
+++ scripts/manage-xe.sh
@@ -131,7 +131,7 @@ else
   /home/oracle/bin/manage-init-xe.sh
 
   enableDBExpress
-  echo "Database ready for use..."
+  echo "DATABASE IS READY TO USE!" #being compatible with testcontainers
   tail -F -n 0 /opt/oracle/diag/rdbms/xe/XE/trace/alert_XE.log
 fi
 
